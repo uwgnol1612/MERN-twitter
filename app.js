f3f6ae55512262f2cc3854d61b1e93716d9e93be
Long McFarlin <longwu2011@gmail.com>
2019-10-29 11:32:03 -0600

    set up user model

--- app.js
+++ app.js
@@ -1,11 +1,39 @@
 const express = require("express");
 const app = express();
 
+const mongoose = require("mongoose");
+const db = require("./config/keys").mongoURI;
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+
+const User = require("./models/User");
+const bodyParser = require("body-parser");
+
+mongoose
+    .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('connect to MongoDB'))
+    .catch(err => console.log(err))
+
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+
+app.use(bodyParser.json());
 
 
 app.get("/", (req, res) => {
+    const user = new User({
+        handle: 'jasmine',
+        email: 'jasmine@gmail.com',
+        password: 'jasmine123'
+    })
+    user.save()
     res.send("Hello a/A");
 })
+
+app.use("/api/users", users)
+app.use("/api/tweets", tweets)
 
 const port = process.env.PORT || 5000;
 
